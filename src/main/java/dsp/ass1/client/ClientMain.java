5d2a4d694e30c27ace0c80dd8879ee8957640651
dorki <dorki@dorki.com>
2016-04-02 17:08:56 +0300

    test

--- src/main/java/dsp/ass1/client/ClientMain.java
+++ src/main/java/dsp/ass1/client/ClientMain.java
@@ -1,7 +1,36 @@
 package dsp.ass1.client;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.Filter;
+import dsp.ass1.utils.InstanceFactory;
+
+import java.util.Arrays;
+
 /**
  * Created by doubled on 0002, 02, 4, 2016.
  */
 public class ClientMain {
+
+    public static void main(String[] args) {
+
+        if(args.length < 2){
+            System.out.println("arguments are missing");
+            return;
+        }
+
+        AWSCredentials credentials = InstanceFactory.getCredentials();
+
+        AmazonEC2Client amazonEC2Client = new AmazonEC2Client(credentials);
+        amazonEC2Client.setEndpoint("ec2.us-east-1.amazonaws.com");
+
+        DescribeInstancesRequest describe = new DescribeInstancesRequest();
+        describe.setFilters(Arrays.asList(new Filter()));
+
+        amazonEC2Client.
+
+    }
 }
