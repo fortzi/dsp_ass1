cd0d7cc816b71f9d4eff652f16613b767ffcb09c
dorki <dorki@dorki.com>
2016-04-02 18:52:53 +0300

    test

---
+++ src/main/java/dsp/ass1/client/ManagerFactory.java
@@ -0,0 +1,16 @@
+package dsp.ass1.client;
+
+import com.amazonaws.auth.AWSCredentials;
+import dsp.ass1.utils.InstanceFactory;
+
+/**
+ * Created by doubled on 0002, 02, 4, 2016.
+ */
+public class ManagerFactory extends InstanceFactory {
+
+    public ManagerFactory(AWSCredentials credentials) {
+        super("manager", credentials);
+
+
+    }
+}
---
+++ src/main/java/dsp/ass1/utils/Constants.java
@@ -0,0 +1,12 @@
+package dsp.ass1.utils;
+
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+
+/**
+ * Created by doubled on 0002, 02, 4, 2016.
+ */
+public class Constants {
+
+    public static Region REGION = Region.getRegion(Regions.US_EAST_1);
+}
